9b9d5aefaaf12b9b50789d85d41c73b5b1750b40
krishnakumarvs <krishh_mea@yahoo.in>
2018-07-29 19:41:49 +0530

    Login and dashboard page created.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { DashboardMainComponent } from './dashboard-main/dashboard-main.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  }, 
+  {
+    path: 'dashboard',
+    component: DashboardMainComponent
   },
   { 
     path: '', 
